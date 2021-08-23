8f59a046260702abfaf8a6194984fac1eae2c066
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2021-08-23 16:59:20 +0000

    Series serialization benchmarks

--- pkg/serializer/json_payload_builder_benchmark_test.go
+++ pkg/serializer/json_payload_builder_benchmark_test.go
@@ -11,8 +11,11 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/DataDog/datadog-agent/pkg/forwarder"
 	"github.com/DataDog/datadog-agent/pkg/metrics"
+	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
 	"github.com/DataDog/datadog-agent/pkg/serializer/stream"
+	"github.com/stretchr/testify/require"
 )
 
 func generateData(points int, items int, tags int) metrics.Series {
@@ -42,70 +45,378 @@ func generateData(points int, items int, tags int) metrics.Series {
 	return series
 }
 
-func benchmarkJSONPayloadBuilderUsage(b *testing.B, points int, items int, tags int) {
+var payloads forwarder.Payloads
+
+func benchmarkSeriesJSerializationUsage(b *testing.B, points int, items int, tags int) {
 	series := generateData(points, items, tags)
 	payloadBuilder := stream.NewJSONPayloadBuilder(true)
 
 	b.ResetTimer()
+	b.ReportAllocs()
 
+	var payloadCount int
+	var payloadCompressedSize uint64
 	for n := 0; n < b.N; n++ {
-		payloadBuilder.Build(series)
+		var err error
+		payloads, err = payloadBuilder.Build(series)
+		payloadCount += len(payloads)
+		for _, pl := range payloads {
+			payloadCompressedSize += uint64(len(*pl))
+		}
+		require.NoError(b, err)
 	}
+	b.ReportMetric(float64(payloadCount)/float64(b.N), "payloads")
+	b.ReportMetric(float64(payloadCompressedSize)/float64(b.N), "compressed-payload-bytes")
 }
 
-func BenchmarkJSONPayloadBuilderThroughputPoints0(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 0, 100, 1)
+func benchmarkSeriesPSerializationUsage(b *testing.B, points int, items int, tags int) {
+	series := generateData(points, items, tags)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	var payloadCount int
+	var payloadCompressedSize uint64
+	for n := 0; n < b.N; n++ {
+		var err error
+		payloads, err = series.MarshalSplitCompress(marshaler.DefaultBufferContext())
+		payloadCount += len(payloads)
+		for _, pl := range payloads {
+			payloadCompressedSize += uint64(len(*pl))
+		}
+		require.NoError(b, err)
+	}
+	b.ReportMetric(float64(payloadCount)/float64(b.N), "payloads")
+	b.ReportMetric(float64(payloadCompressedSize)/float64(b.N), "compressed-payload-bytes")
 }
-func BenchmarkJSONPayloadBuilderThroughputPoints1(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 1)
+func BenchmarkSeriesP_000005Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 5, 10)
 }
-func BenchmarkJSONPayloadBuilderThroughputPoints2(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 2, 100, 1)
+func BenchmarkSeriesP_000005Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 5, 50)
 }
-func BenchmarkJSONPayloadBuilderThroughputPoints5(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 5, 100, 1)
+func BenchmarkSeriesP_000005Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 5, 10)
 }
-func BenchmarkJSONPayloadBuilderThroughputPoints10(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 10, 100, 1)
+func BenchmarkSeriesP_000005Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 5, 50)
 }
-func BenchmarkJSONPayloadBuilderThroughputPoints100(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 100, 100, 1)
+func BenchmarkSeriesP_000005Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 5, 10)
+}
+func BenchmarkSeriesP_000005Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 5, 50)
+}
+func BenchmarkSeriesP_000005Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 5, 10)
+}
+func BenchmarkSeriesP_000005Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 5, 50)
+}
+func BenchmarkSeriesP_000010Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 10, 10)
+}
+func BenchmarkSeriesP_000010Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 10, 50)
+}
+func BenchmarkSeriesP_000010Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 10, 10)
+}
+func BenchmarkSeriesP_000010Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 10, 50)
+}
+func BenchmarkSeriesP_000010Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 10, 10)
+}
+func BenchmarkSeriesP_000010Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 10, 50)
+}
+func BenchmarkSeriesP_000010Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 10, 10)
+}
+func BenchmarkSeriesP_000010Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 10, 50)
+}
+func BenchmarkSeriesP_000100Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 100, 10)
+}
+func BenchmarkSeriesP_000100Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 100, 50)
+}
+func BenchmarkSeriesP_000100Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 100, 10)
+}
+func BenchmarkSeriesP_000100Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 100, 50)
+}
+func BenchmarkSeriesP_000100Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 100, 10)
+}
+func BenchmarkSeriesP_000100Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 100, 50)
+}
+func BenchmarkSeriesP_000100Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 100, 10)
+}
+func BenchmarkSeriesP_000100Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 100, 50)
+}
+func BenchmarkSeriesP_000500Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 100, 10)
+}
+func BenchmarkSeriesP_000500Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 100, 50)
+}
+func BenchmarkSeriesP_000500Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 100, 10)
+}
+func BenchmarkSeriesP_000500Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 100, 50)
+}
+func BenchmarkSeriesP_000500Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 100, 10)
+}
+func BenchmarkSeriesP_000500Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 100, 50)
+}
+func BenchmarkSeriesP_000500Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 100, 10)
+}
+func BenchmarkSeriesP_000500Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 100, 50)
+}
+func BenchmarkSeriesP_001000Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 1000, 10)
+}
+func BenchmarkSeriesP_001000Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 1000, 50)
+}
+func BenchmarkSeriesP_001000Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 1000, 10)
+}
+func BenchmarkSeriesP_001000Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 1000, 50)
+}
+func BenchmarkSeriesP_001000Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 1000, 10)
+}
+func BenchmarkSeriesP_001000Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 1000, 50)
+}
+func BenchmarkSeriesP_001000Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 1000, 10)
+}
+func BenchmarkSeriesP_001000Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 1000, 50)
+}
+func BenchmarkSeriesP_010000Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 10000, 10)
+}
+func BenchmarkSeriesP_010000Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 10000, 50)
+}
+func BenchmarkSeriesP_010000Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 10000, 10)
+}
+func BenchmarkSeriesP_010000Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 10000, 50)
+}
+func BenchmarkSeriesP_010000Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 10000, 10)
+}
+func BenchmarkSeriesP_010000Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 10000, 50)
+}
+func BenchmarkSeriesP_010000Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 10000, 10)
+}
+func BenchmarkSeriesP_010000Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 10000, 50)
+}
+func BenchmarkSeriesP_100000Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 100000, 10)
+}
+func BenchmarkSeriesP_100000Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 5, 100000, 50)
+}
+func BenchmarkSeriesP_100000Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 100000, 10)
+}
+func BenchmarkSeriesP_100000Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 10, 100000, 50)
+}
+func BenchmarkSeriesP_100000Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 100000, 10)
+}
+func BenchmarkSeriesP_100000Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 100, 100000, 50)
+}
+func BenchmarkSeriesP_100000Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesPSerializationUsage(b, 1000, 100000, 10)
 }
 
-func BenchmarkJSONPayloadBuilderTags0(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 0)
+func BenchmarkSeriesJ_000005Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 5, 10)
 }
-func BenchmarkJSONPayloadBuilderTags1(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 1)
+func BenchmarkSeriesJ_000005Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 5, 50)
 }
-func BenchmarkJSONPayloadBuilderTags2(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 2)
+func BenchmarkSeriesJ_000005Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 5, 10)
 }
-func BenchmarkJSONPayloadBuilderTags5(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 5)
+func BenchmarkSeriesJ_000005Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 5, 50)
 }
-func BenchmarkJSONPayloadBuilderTags10(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 10)
+func BenchmarkSeriesJ_000005Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 5, 10)
 }
-func BenchmarkJSONPayloadBuilderTags100(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 100)
+func BenchmarkSeriesJ_000005Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 5, 50)
 }
-
-func BenchmarkJSONPayloadBuilderItems1(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 1, 1)
+func BenchmarkSeriesJ_000005Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 5, 10)
 }
-func BenchmarkJSONPayloadBuilderItems10(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 10, 1)
+func BenchmarkSeriesJ_000005Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 5, 50)
 }
-func BenchmarkJSONPayloadBuilderItems100(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 1)
+func BenchmarkSeriesJ_000010Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 10, 10)
 }
-func BenchmarkJSONPayloadBuilderItems1000(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 1000, 1)
+func BenchmarkSeriesJ_000010Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 10, 50)
 }
-func BenchmarkJSONPayloadBuilderItems10000(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 10000, 1)
+func BenchmarkSeriesJ_000010Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 10, 10)
 }
-func BenchmarkJSONPayloadBuilderItems100000(b *testing.B) {
-	benchmarkJSONPayloadBuilderUsage(b, 1, 100000, 1)
+func BenchmarkSeriesJ_000010Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 10, 50)
+}
+func BenchmarkSeriesJ_000010Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 10, 10)
+}
+func BenchmarkSeriesJ_000010Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 10, 50)
+}
+func BenchmarkSeriesJ_000010Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 10, 10)
+}
+func BenchmarkSeriesJ_000010Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 10, 50)
+}
+func BenchmarkSeriesJ_000100Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 100, 10)
+}
+func BenchmarkSeriesJ_000100Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 100, 50)
+}
+func BenchmarkSeriesJ_000100Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 100, 10)
+}
+func BenchmarkSeriesJ_000100Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 100, 50)
+}
+func BenchmarkSeriesJ_000100Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 100, 10)
+}
+func BenchmarkSeriesJ_000100Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 100, 50)
+}
+func BenchmarkSeriesJ_000100Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 100, 10)
+}
+func BenchmarkSeriesJ_000100Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 100, 50)
+}
+func BenchmarkSeriesJ_000500Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 100, 10)
+}
+func BenchmarkSeriesJ_000500Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 100, 50)
+}
+func BenchmarkSeriesJ_000500Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 100, 10)
+}
+func BenchmarkSeriesJ_000500Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 100, 50)
+}
+func BenchmarkSeriesJ_000500Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 100, 10)
+}
+func BenchmarkSeriesJ_000500Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 100, 50)
+}
+func BenchmarkSeriesJ_000500Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 100, 10)
+}
+func BenchmarkSeriesJ_000500Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 100, 50)
+}
+func BenchmarkSeriesJ_001000Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 1000, 10)
+}
+func BenchmarkSeriesJ_001000Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 1000, 50)
+}
+func BenchmarkSeriesJ_001000Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 1000, 10)
+}
+func BenchmarkSeriesJ_001000Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 1000, 50)
+}
+func BenchmarkSeriesJ_001000Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 1000, 10)
+}
+func BenchmarkSeriesJ_001000Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 1000, 50)
+}
+func BenchmarkSeriesJ_001000Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 1000, 10)
+}
+func BenchmarkSeriesJ_001000Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 1000, 50)
+}
+func BenchmarkSeriesJ_010000Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 10000, 10)
+}
+func BenchmarkSeriesJ_010000Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 10000, 50)
+}
+func BenchmarkSeriesJ_010000Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 10000, 10)
+}
+func BenchmarkSeriesJ_010000Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 10000, 50)
+}
+func BenchmarkSeriesJ_010000Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 10000, 10)
+}
+func BenchmarkSeriesJ_010000Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 10000, 50)
+}
+func BenchmarkSeriesJ_010000Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 10000, 10)
+}
+func BenchmarkSeriesJ_010000Items_001000Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 10000, 50)
+}
+func BenchmarkSeriesJ_100000Items_000005Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 100000, 10)
+}
+func BenchmarkSeriesJ_100000Items_000005Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 5, 100000, 50)
+}
+func BenchmarkSeriesJ_100000Items_000010Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 100000, 10)
+}
+func BenchmarkSeriesJ_100000Items_000010Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 10, 100000, 50)
+}
+func BenchmarkSeriesJ_100000Items_000100Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 100000, 10)
+}
+func BenchmarkSeriesJ_100000Items_000100Points_000050Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 100, 100000, 50)
+}
+func BenchmarkSeriesJ_100000Items_001000Points_000010Tags(b *testing.B) {
+	benchmarkSeriesJSerializationUsage(b, 1000, 100000, 10)
 }
